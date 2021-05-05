0a5bb7c185ad3ff341853e1d9c36616ff32765c6
Masataka Nishimori <nishimori-m@mnes.org>
2021-05-05 15:08:51 +0900

    Add install description

--- shopee/__init__.py
+++ shopee/__init__.py
@@ -5,7 +5,6 @@ from . import image_embeddings_pytorch
 from . import image_embeddings_keras
 from . import image_embeddings
 from . import text_embeddings
-from . import candidate_extraction
 from . import matching
 from . import training
 from . import optimization
